82838cb492c91aed48ab4c8d814881c678e3cca6
MayanzaGo <gaelmayanza@gmail.com>
2025-07-18 08:08:27 -0400

    Add files via upload
    Initial project upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# personality-database-project
-A structured MySQL database for personality prediction
+
+# Personality Prediction Database (MySQL)
+
+A structured and normalized MySQL database for behavioral analytics and personality classification (Introvert vs. Extrovert). Designed to support data science and machine learning workflows.
+
+
+## Database Schema
+- **person**: Personal info (name, gender, age)
+- **behavior**: Psychological traits (talkative, empathy, etc.)
+- **personality_label**: Final personality classification
+
+## Technologies Used
+- MySQL
+- Power BI (ER Diagram)
+- SQL (DDL & Constraints)
+
+## ER Diagram
+ER diagram created using Power BI:
+
+
+##  Files
+- `schema.sql`: SQL script to create and define tables
+- `personality_dataset_synthetic.csv`: Synthetic dataset for testing
+
+## Project Purpose
+
+This database helps:
+- Behavioral scientists explore trait correlations
+- Data scientists prepare labeled datasets for personality prediction
+- Dashboards to monitor behavioral patterns
---
+++ Schema_personality.sql
@@ -0,0 +1,42 @@
+create database personality;
+use personality;
+
+
+# Step 1 create and load dataset
+
+-- Person
+create table person (
+       person_id bigint primary key,
+       name varchar(100),
+       gender enum('Male', 'Female'),
+       age int
+);
+
+select *
+from person;
+
+-- Behavior_scores  
+create table behavior (
+        behavior_scores_number bigint primary key,
+        selfesteem tinyint unsigned check (selfesteem between 1  and 10),
+        talkative tinyint unsigned check (talkative between 1  and 10),
+        adventurous tinyint unsigned check (adventurous between 1  and 10),
+        passive tinyint unsigned check (passive between 1  and 10),
+        empathy tinyint unsigned check (empathy between 1  and 10),
+        person_id bigint not null,  
+        foreign key (person_id) references person(person_id)
+);
+select *
+from behavior;
+
+
+-- Personality label or target
+create table personality_lable (
+		personality_lable_numer bigint primary key,
+        personality enum('Introvert', 'Extrovert'),
+        person_id bigint not null,
+        foreign key (person_id) references person(person_id)
+);
+
+select *
+from personality_lable 
